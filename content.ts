98aaae303f9bafe0c9db7d18af44899a63387086
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-08-24 16:23:06 +0200

    added elo effect and added popup
    you can now see the elo effect for wins, losses and draws

--- content.ts
+++ content.ts
@@ -1,7 +1,16 @@
 const url = window.location.href;
 const u = url.split("/")[4];
 console.log(url);
+
 console.log("chess.stjo.dev/api/bot/" + u + "/");
+
+function roundAndFormat(number) {
+  const roundedNumber = Math.round(number);
+  const formattedNumber =
+    roundedNumber >= 0 ? `+${roundedNumber}` : `${roundedNumber}`;
+  return formattedNumber;
+}
+
 fetch("https://chess.stjo.dev/api/bot/" + u + "/")
   .then((res) => res.json())
   .then((r) => {
@@ -12,10 +21,20 @@ fetch("https://chess.stjo.dev/api/bot/" + u + "/")
     var wins = 0;
     var losses = 0;
     var draws = 0;
+    var winEloEffect = 0;
+    var loseEloEffect = 0;
+    var drawEloEffect = 0;
     games.forEach((game) => {
-      if (game.winner == "d") draws++;
-      else if (game[game.winner + "name"] == name) wins++;
-      else losses++;
+      if (game.winner == "d") {
+        draws++;
+        drawEloEffect += game.elo_change;
+      } else if (game[game.winner + "name"] == name) {
+        wins++;
+        winEloEffect += game.elo_change;
+      } else {
+        losses++;
+        loseEloEffect += game.elo_change;
+      }
     });
 
     console.log(`${wins} wins ${losses} losses ${draws} draws`);
@@ -38,25 +57,82 @@ fetch("https://chess.stjo.dev/api/bot/" + u + "/")
 
     const statElem = document.createElement("div");
 
-    statElem.id = "stats-panel";
+    statElem.id = "stat-panel";
+    statElem.innerHTML = `
+
+    <div id="counters">
+    <div class="m5">
+    <h2 class="m5">Wins</h2>
+    <p class="counter m5">${wins}</p>
+    <p class="counter elo-effect "> ${roundAndFormat(winEloEffect)} elo</p>
+    
+  </div>
+  <div class="m5">
+    <h2 class="m5">Losses</h2>
+    <p class="counter m5">${losses}</p>
+    <p class="counter elo-effect "> ${roundAndFormat(loseEloEffect)} elo</p>
+    
+  </div>
+  <div class="m5">
+    <h2 class="m5">Draws</h2>
+    <p class="counter m5">${draws}</p>
+    <p class="counter elo-effect"> ${roundAndFormat(drawEloEffect)} elo</p>
+  </div>
+</div>
+  </div>
+  <button id="more-button">...</button>
+  <div id="bgdarkener">
+    <div id="stat-popup">
+      <div class="flex">
+        <div class="m5">
+          <h2 class="m5">Wins</h2>
+          <p class="counter m5">${wins}</p>
+          <p class="counter m5"> +53 elo</p>
+          
+        </div>
+        <div class="m5">
+          <h2 class="m5">Losses</h2>
+          <p class="counter m5">${losses}</p>
+          <p class="counter m5"> +53 elo</p>
+          
+        </div>
+        <div class="m5">
+          <h2 class="m5">Draws</h2>
+          <p class="counter m5">${draws}</p>
+          <p class="counter m5"> +53 elo</p>
+        </div>
+      </div>
+      <div class="grid">
+        <div class="flex elo-effect space-gap">
+          <p class="bold">Elo effect by wins:<p>
+          <p>+24</p>
+        </div>
+        <div class="flex elo-effect space-gap">
+          <p class="bold">Elo effect by loses:<p>
+          <p>+24</p>
+        </div>
+        
+        
+        
+      </div>
+
+      <button id="close-button">x</button>
+    </div>
+  </div>
+
+    `;
 
     newParentElement.appendChild(statElem);
 
-    statElem.innerHTML = `
-      <div style="margin: 5px">
-        <h2 style="margin: 5px">Wins</h2>
-        <p class="counter">${wins}</p>
-      </div>
-      <div style="margin: 5px">
-        <h2 style="margin: 5px">Losses</h2>
-        <p class="counter">${losses}</p>
-      </div>
-      <div style="margin: 5px">
-        <h2 style="margin: 5px">Draws</h2>
-        <p class="counter">${draws}</p>
-      </div>
-      ;
-    `;
+    const statPopup = document.getElementById("bgdarkener");
+
+    if (!statPopup) return;
+    document.getElementById("more-button")?.addEventListener("click", () => {
+      statPopup.style.display = "flex";
+    });
+    document.getElementById("close-button")?.addEventListener("click", () => {
+      statPopup.style.display = "none";
+    });
 
     // setTimeout(() => {
     // }, 200);
