c984041ad954a5ad47486f19ef2cfdb19cc2e560
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-08-27 20:27:47 +0200

    colored elo change text

--- content.ts
+++ content.ts
@@ -4,6 +4,9 @@ console.log(url);
 
 console.log("chess.stjo.dev/api/bot/" + u + "/");
 
+const red = [255, 0, 0];
+const green = [0, 255, 0];
+
 function roundAndFormat(number, decimal = 1) {
   const roundedNumber = round(number, decimal);
   const formattedNumber =
@@ -40,8 +43,9 @@ fetch("https://chess.stjo.dev/api/bot/" + u + "/")
     };
 
     const nameElements = document.querySelectorAll(".name");
+    const EloChangeElements = document.getElementsByClassName("elo");
 
-    console.log(nameElements.length);
+    console.log(EloChangeElements.length);
     games.forEach((game, index) => {
       var ending = 0;
       if (game.winner == "d") {
@@ -78,6 +82,19 @@ fetch("https://chess.stjo.dev/api/bot/" + u + "/")
           "winner-text"
         );
       }
+      const eloColorFactor = Math.min(
+        Math.max(game.elo_change / 40 + 0.5, 0),
+        1
+      );
+      const eloColor = [
+        Math.round(red[0] * (1 - eloColorFactor) + green[0] * eloColorFactor), // R
+        Math.round(red[1] * (1 - eloColorFactor) + green[1] * eloColorFactor), // G
+        Math.round(red[2] * (1 - eloColorFactor) + green[2] * eloColorFactor), // B
+      ];
+
+      const eloColorString = `rgb(${eloColor[0]}, ${eloColor[1]}, ${eloColor[2]})`;
+
+      EloChangeElements[index].style.color = eloColorString;
     });
 
     console.log(`${wins} wins ${losses} losses ${draws} draws`);
